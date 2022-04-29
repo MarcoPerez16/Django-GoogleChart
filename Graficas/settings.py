4be7cc48b62af3158b2ba9678a7dc7829f977184
MarcoPerez551 <A01660337@itesm.mx>
2022-04-28 23:32:49 -0500

    Final Commit

--- Graficas/settings.py
+++ Graficas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#20lvc84y)sl5^qg*+d*l9ffga-vxt14^pjq$tlxs%^8i+((e-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
